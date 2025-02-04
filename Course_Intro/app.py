c2eb0b41c7569bfaa9ffbe94bdcd91399263c735
Vishwajeet Mehta <vishwajeet25200012@gmail.com>
2025-02-04 21:06:18 +0530

    Worked on different heading and markdowns

--- Course_Intro/app.py
+++ Course_Intro/app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
-st.title('Hello Geeks, Welcome to Geeks for Geeks')
+st.title('Title  ->  Hello Geeks, Welcome to Geeks for Geeks')   #Title
+st.header('Header  ->  Geeks for Geeks')      #Header
+st.subheader('Subheader  ->  Geeks for Geeks')      #SubHeader
+st.text('Text  ->  Geeks for Geeks')                #Text
+
+# Markdown
+st.markdown('# Hi')             
+st.markdown('## Hi')
+st.markdown('### Hi')
+st.markdown('#### Hi')
+st.markdown('##### Hi')
+st.markdown('Hi')
 
