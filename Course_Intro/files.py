ea2dedc69420b3584b4ce4391116b189660359cb
Vishwajeet Mehta <vishwajeet25200012@gmail.com>
2025-02-05 22:52:08 +0530

    Done with files.py

--- Course_Intro/files.py
+++ Course_Intro/files.py
@@ -16,14 +16,23 @@ st.subheader('Dealing with images')
 #st.image('img.png')        #Directly
 img_file = st.file_uploader('Upload the IMG file',type = ['png','jpeg']) #By Uploading
 if img_file is not None:
-    st.image('img.png')
+    st.image(img_file)
 
 st.subheader('Dealing with videos')
 #Directly
-st.video()
+#st.video('video.mp4')
 #Uploading
+vid_file = st.file_uploader('Upload the video',type = ['mp4'])
+if vid_file is not None:
+    st.video(vid_file,start_time = 0)
 
-
+st.subheader('Dealing with audios')
+#Directly
+#st.audio('song.mp3')
+#Uploading
+aud_file = st.file_uploader('Upload the audio',type = ['mp3'])
+if aud_file is not None:
+    st.audio(aud_file.read(),start_time = 10)
 
 
 
